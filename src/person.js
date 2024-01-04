19513fc55e11266388ca000ea8dd6c09db27e804
C5363386 <joao.vitor.brietzke.de.lima@sap.com>
2024-01-04 20:32:31 -0300

    feat: starting person

---
+++ src/person.js
@@ -0,0 +1,22 @@
+export default class Person {
+  constructor({ id, vehicles, kmTraveled, from, to }) {
+    this.id = id;
+    this.vehicles = vehicles;
+    this.kmTraveled = kmTraveled;
+    this.from = from;
+    this.to = to;
+  }
+
+  formatted(language) {
+    return {
+      id: Number(this.id),
+      vehicles: new Intl.listFormat(language, {
+        style: "long",
+        type: "conjunction",
+      }).format(this.vehicles),
+      //   kmTraveled:
+      // from
+      // to
+    };
+  }
+}
