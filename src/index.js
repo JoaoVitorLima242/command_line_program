b7235990fed368026aec5abfc1aac504ea7d4a33
C5363386 <joao.vitor.brietzke.de.lima@sap.com>
2024-01-05 15:05:27 -0300

    feat: creating a looping in terminal

--- src/index.js
+++ src/index.js
@@ -2,6 +2,25 @@ import TerminalController from "./terminalController.js";
 import database from "./../database.json" assert { type: "json" };
 
 const LANGUAGE = "pt-br";
+const STOP_TERM = ":q";
 
 const terminalController = new TerminalController();
 terminalController.initializeTerminal(database, LANGUAGE);
+
+async function mainLoop() {
+  try {
+    const answer = await terminalController.question("what");
+    if (answer === STOP_TERM) {
+      terminalController.closeTerminal();
+      console.log("Process finished!");
+      return;
+    }
+
+    return mainLoop();
+  } catch (e) {
+    console.log("Error: " + e.message);
+    return mainLoop();
+  }
+}
+
+await mainLoop();
