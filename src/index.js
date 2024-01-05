196cb49ca45f66024199256f3f9666450e765256
C5363386 <joao.vitor.brietzke.de.lima@sap.com>
2024-01-05 14:59:38 -0300

    feat: creating a terminal controller

--- src/index.js
+++ src/index.js
@@ -1,31 +1,7 @@
-import DraftLog from "draftlog";
-import chalk from "chalk";
-import chalkTable from "chalk-table";
-import readline from "readline";
+import TerminalController from "./terminalController.js";
+import database from "./../database.json" assert { type: "json" };
 
-import database from "../database.json" assert { type: "json" };
-import Person from "./person.js";
+const LANGUAGE = "pt-br";
 
-DraftLog(console).addLineListener(process.stdin);
-
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: "id", name: chalk.cyan("ID") },
-    { field: "vehicles", name: chalk.magenta("Vehicles") },
-    { field: "kmTraveled", name: chalk.cyan("Km Traveled") },
-    { field: "from", name: chalk.cyan("From") },
-    { field: "to", name: chalk.cyan("To") },
-  ],
-};
-
-const table = chalkTable(
-  options,
-  database.map((item) => new Person(item).formatted("pt-br"))
-);
-const print = console.draft(table);
-
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, LANGUAGE);
